f48d77ae729066df3b9bad3ad3d3bd7844ce386a
zsh <zsh@alpha-pc>
2014-12-28 11:51:29 +0800

    init

---
+++ web/src/main/java/com/frog/core/mvc/binder/WebBinder.java
@@ -0,0 +1,20 @@
+package com.frog.core.mvc.binder;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.support.WebBindingInitializer;
+import org.springframework.web.context.request.WebRequest;
+
+public class WebBinder implements WebBindingInitializer {
+
+	@Override
+	public void initBinder(WebDataBinder binder, WebRequest request) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		dateFormat.setLenient(false);
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
+	}
+
+}
